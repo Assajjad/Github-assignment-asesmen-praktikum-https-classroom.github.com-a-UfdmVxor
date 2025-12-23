bce20c7c392bb7918eba8ff8a8f11078259290e8
assajjad <assajjad04@gmail.com>
2025-12-23 11:00:38 +0700

    Nyerah pak

---
+++ Soal1.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Soal1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String kalimat = sc.nextLine(); 
+    }
+}
---
+++ Soal3.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Soal3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int [][] isi = new int[n][n];
+        
+        for (int i = 0; i<n; i++) {
+            int nilai = sc.nextInt();
+        }
+        
+        
+        for (int i=0; i<n; i++) {
+            System.out.println("Cabang " + (i+1) ) ;
+        }
+    }
+}
